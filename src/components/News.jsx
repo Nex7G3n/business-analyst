2ef5827f8c24b294da13991b1a6471330926446a
inmari <106024243+iguevarav@users.noreply.github.com>
2024-10-14 23:59:29 -0500

    Add files via upload

---
+++ src/components/News.jsx
@@ -0,0 +1,85 @@
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import './App.css';
+import 'font-awesome/css/font-awesome.min.css';
+
+function News() {
+  const [news, setNews] = useState([]);
+  const [error, setError] = useState(null);
+  const [searchTerm, setSearchTerm] = useState(''); //para guardar lo que se va a buscar
+  const [visibleCount, setVisibleCount] = useState(9); //Mostrar 12 articulos inicialmente
+
+  useEffect(() => {
+    const apiKey = '75a1603eecb042598d28343256620698'; // Reemplaza con tu propia API Key de NewsAPI
+    const url = `https://newsapi.org/v2/everything?q=finance&language=es&sortBy=publishedAt&apiKey=${apiKey}`;
+
+    axios.get(url)
+      .then(response => {
+        setNews(response.data.articles);
+      })
+      .catch(error => {
+        setError('Error al obtener las noticias.');
+        console.error('Error al hacer la solicitud:', error);
+      });
+  }, []);
+
+  if (error) {
+    return <div>{error}</div>;
+  }
+
+  const getIcon = (sourceName) => {
+    const icons = {
+      'CNN': 'fa-newspaper-o',
+      'BBC': 'fa-globe',
+    };
+    return icons[sourceName] || 'fa-globe';
+  };
+
+  const filteredNews = news.filter(article => {
+    return (
+      article.title.toLowerCase().includes(searchTerm.toLowerCase()) ||
+      article.description.toLowerCase().includes(searchTerm.toLowerCase())
+    );
+  });
+
+  const loadMore = () => {
+    setVisibleCount(prevCount => prevCount + 12);
+  }
+
+  return (
+    <div className="news-container">
+      <h2>Últimas noticias sobre Finanzas</h2>
+
+      <div className='search-container'>
+        <i className='fa fa-search search-icon fa-lg'></i>
+        <input type="text" placeholder="Search ..." value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
+          className="search-input" />
+      </div>
+
+      <div className="news-grid">
+      {filteredNews.slice(0, visibleCount).map((article, index) => (
+          <div key={index} className="news-card">
+            <a href={article.url} target="_blank" rel="noopener noreferrer">
+              <img src={article.urlToImage} alt={article.title} className="news-image" />
+              <div className="news-source">
+                <i className={`fa ${getIcon(article.source.name)}`}></i>{' '}
+                <span>{article.source.name}</span>
+              </div>
+              <h3 className="news-title">{article.title}</h3>
+              <p className="news-description">{article.description}</p>
+            </a>
+          </div>
+        ))}
+      </div>
+
+      {filteredNews.length > visibleCount && ( 
+        <button onClick={loadMore} className="load-more-button">Mostrar más</button>
+      )}
+
+    </div>
+  );
+}
+
+export default News;
+
