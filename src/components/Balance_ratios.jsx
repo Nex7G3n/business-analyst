48e7acf7870ff64238019c5575f05aeaab9dbb40
gerson08-07 <pulachegerson@gmail.com>
2024-10-18 02:48:59 -0500

    Mejorando interfaz

--- src/components/Balance_ratios.jsx
+++ src/components/Balance_ratios.jsx
@@ -3,7 +3,6 @@ import {
   TableBody,
   TableCaption,
   TableCell,
-  TableFooter,
   TableHead,
   TableHeader,
   TableRow,
@@ -13,31 +12,32 @@ import React from 'react';
 
 const BalanceFinanciero = ({ balance, ratios }) => {
   return (
-    <div>
+    <div className="space-y-6 grid justify-center my-10">
       {balance && (
         <>
-          <Table>
+          <Table className="w-[1100px]">
             <TableCaption>Balance Financiero</TableCaption>
             <TableHeader>
               <TableRow>
-                <TableHead className="w-[300px]">Item</TableHead>
-                <TableHead className="text-right">Valor (USD)</TableHead>
+                <TableHead className="w-[300px] text-lg">Item</TableHead>
+                <TableHead className="text-right text-lg">Valor (USD)</TableHead>
               </TableRow>
             </TableHeader>
             <TableBody>
+              {/* ACTIVO */}
               <TableRow>
-                <TableCell className="font-medium text-left" colSpan={2}>
+                <TableCell className="font-bold text-left bg-gray-100" colSpan={2}>
                   ACTIVO
                 </TableCell>
               </TableRow>
               <TableRow>
-                <TableCell className="text-left">Disponible</TableCell>
+                <TableCell className="pl-8 text-left">Disponible</TableCell>
                 <TableCell className="text-right">
                   ${balance.disponible.toLocaleString("en-US")}
                 </TableCell>
               </TableRow>
               <TableRow>
-                <TableCell className="text-left">
+                <TableCell className="pl-8 text-left">
                   Bancos y Otras Empresas del Sistema FII
                 </TableCell>
                 <TableCell className="text-right">
@@ -45,13 +45,13 @@ const BalanceFinanciero = ({ balance, ratios }) => {
                 </TableCell>
               </TableRow>
               <TableRow>
-                <TableCell className="text-left">Otras Disponibilidades</TableCell>
+                <TableCell className="pl-8 text-left">Otras Disponibilidades</TableCell>
                 <TableCell className="text-right">
                   ${balance.otrasDisponibilidades.toLocaleString("en-US")}
                 </TableCell>
               </TableRow>
               <TableRow>
-                <TableCell className="font-medium text-left">
+                <TableCell className="pl-8 font-medium text-left">
                   Cartera de Créditos
                 </TableCell>
                 <TableCell className="text-right">
@@ -59,13 +59,13 @@ const BalanceFinanciero = ({ balance, ratios }) => {
                 </TableCell>
               </TableRow>
               <TableRow>
-                <TableCell className="text-left">Créditos Vigentes</TableCell>
+                <TableCell className="pl-16 text-left">Créditos Vigentes</TableCell>
                 <TableCell className="text-right">
                   ${balance.creditosVigentes.toLocaleString("en-US")}
                 </TableCell>
               </TableRow>
               <TableRow>
-                <TableCell className="text-left">
+                <TableCell className="pl-16 text-left">
                   Créditos en Cobranza Judicial
                 </TableCell>
                 <TableCell className="text-right">
@@ -73,7 +73,7 @@ const BalanceFinanciero = ({ balance, ratios }) => {
                 </TableCell>
               </TableRow>
               <TableRow>
-                <TableCell className="text-left">
+                <TableCell className="pl-16 text-left">
                   Provisiones para Créditos
                 </TableCell>
                 <TableCell className="text-right">
@@ -81,7 +81,7 @@ const BalanceFinanciero = ({ balance, ratios }) => {
                 </TableCell>
               </TableRow>
               <TableRow>
-                <TableCell className="text-left">
+                <TableCell className="pl-16 text-left">
                   Inmuebles, Mobiliario y Equipo
                 </TableCell>
                 <TableCell className="text-right">
@@ -89,21 +89,22 @@ const BalanceFinanciero = ({ balance, ratios }) => {
                 </TableCell>
               </TableRow>
               <TableRow>
-                <TableCell className="font-medium text-left">
+                <TableCell className="pl-8 font-bold text-left">
                   Total Activo
                 </TableCell>
-                <TableCell className="text-right">
+                <TableCell className="text-right font-bold">
                   ${balance.totalActivo.toLocaleString("en-US")}
                 </TableCell>
               </TableRow>
 
+              {/* PASIVO */}
               <TableRow>
-                <TableCell className="font-medium text-left" colSpan={2}>
+                <TableCell className="font-bold text-left bg-gray-100" colSpan={2}>
                   PASIVO
                 </TableCell>
               </TableRow>
               <TableRow>
-                <TableCell className="text-left">
+                <TableCell className="pl-8 text-left">
                   Obligaciones con los Asociados
                 </TableCell>
                 <TableCell className="text-right">
@@ -111,7 +112,7 @@ const BalanceFinanciero = ({ balance, ratios }) => {
                 </TableCell>
               </TableRow>
               <TableRow>
-                <TableCell className="text-left">
+                <TableCell className="pl-8 text-left">
                   Obligaciones por Cuentas de Ahorro
                 </TableCell>
                 <TableCell className="text-right">
@@ -119,7 +120,7 @@ const BalanceFinanciero = ({ balance, ratios }) => {
                 </TableCell>
               </TableRow>
               <TableRow>
-                <TableCell className="text-left">
+                <TableCell className="pl-8 text-left">
                   Obligaciones por Cuenta a Plazo
                 </TableCell>
                 <TableCell className="text-right">
@@ -127,19 +128,19 @@ const BalanceFinanciero = ({ balance, ratios }) => {
                 </TableCell>
               </TableRow>
               <TableRow>
-                <TableCell className="text-left">Gastos por Pagar</TableCell>
+                <TableCell className="pl-8 text-left">Gastos por Pagar</TableCell>
                 <TableCell className="text-right">
                   ${balance.gastosPorPagar.toLocaleString("en-US")}
                 </TableCell>
               </TableRow>
               <TableRow>
-                <TableCell className="text-left">Cuentas por Pagar</TableCell>
+                <TableCell className="pl-8 text-left">Cuentas por Pagar</TableCell>
                 <TableCell className="text-right">
                   ${balance.cuentasPorPagar.toLocaleString("en-US")}
                 </TableCell>
               </TableRow>
               <TableRow>
-                <TableCell className="text-left">
+                <TableCell className="pl-8 text-left">
                   Provisiones Asociativas
                 </TableCell>
                 <TableCell className="text-right">
@@ -147,33 +148,34 @@ const BalanceFinanciero = ({ balance, ratios }) => {
                 </TableCell>
               </TableRow>
               <TableRow>
-                <TableCell className="font-medium text-left">
+                <TableCell className="pl-8 font-bold text-left">
                   Total del Pasivo
                 </TableCell>
-                <TableCell className="text-right">
+                <TableCell className="text-right font-bold">
                   ${balance.totalPasivo.toLocaleString("en-US")}
                 </TableCell>
               </TableRow>
 
+              {/* PATRIMONIO */}
               <TableRow>
-                <TableCell className="font-medium text-left" colSpan={2}>
+                <TableCell className="font-bold text-left bg-gray-100" colSpan={2}>
                   PATRIMONIO
                 </TableCell>
               </TableRow>
               <TableRow>
-                <TableCell className="text-left">Capital Social</TableCell>
+                <TableCell className="pl-8 text-left">Capital Social</TableCell>
                 <TableCell className="text-right">
                   ${balance.capitalSocial.toLocaleString("en-US")}
                 </TableCell>
               </TableRow>
               <TableRow>
-                <TableCell className="text-left">Reservas</TableCell>
+                <TableCell className="pl-8 text-left">Reservas</TableCell>
                 <TableCell className="text-right">
                   ${balance.reservas.toLocaleString("en-US")}
                 </TableCell>
               </TableRow>
               <TableRow>
-                <TableCell className="text-left">
+                <TableCell className="pl-8 text-left">
                   Resultados Acumulados
                 </TableCell>
                 <TableCell className="text-right">
@@ -181,7 +183,7 @@ const BalanceFinanciero = ({ balance, ratios }) => {
                 </TableCell>
               </TableRow>
               <TableRow>
-                <TableCell className="text-left">
+                <TableCell className="pl-8 text-left">
                   Resultado Neto del Ejercicio
                 </TableCell>
                 <TableCell className="text-right">
@@ -189,16 +191,17 @@ const BalanceFinanciero = ({ balance, ratios }) => {
                 </TableCell>
               </TableRow>
               <TableRow>
-                <TableCell className="font-medium text-left">
+                <TableCell className="pl-8 font-bold text-left">
                   Total del Patrimonio
                 </TableCell>
-                <TableCell className="text-right">
+                <TableCell className="text-right font-bold">
                   ${balance.totalPatrimonio.toLocaleString("en-US")}
                 </TableCell>
               </TableRow>
 
+              {/* TOTAL PASIVO Y PATRIMONIO */}
               <TableRow>
-                <TableCell className="font-medium text-left">
+                <TableCell className="pl-8 font-bold text-left">
                   Total del Pasivo y Patrimonio
                 </TableCell>
                 <TableCell className="text-right">
@@ -217,11 +220,12 @@ const BalanceFinanciero = ({ balance, ratios }) => {
           <TableCaption>Ratios Financieros</TableCaption>
           <TableHeader>
             <TableRow>
-              <TableHead className="w-[200px]">Ratio</TableHead>
-              <TableHead className="text-right">Valor</TableHead>
+              <TableHead className="w-[200px] text-lg">Ratio</TableHead>
+              <TableHead className="text-right text-lg">Valor</TableHead>
             </TableRow>
           </TableHeader>
           <TableBody>
+            {/* Ratios */}
             <TableRow>
               <TableCell className="font-medium text-left">PER</TableCell>
               <TableCell className="text-right">{ratios.per}</TableCell>
@@ -236,47 +240,11 @@ const BalanceFinanciero = ({ balance, ratios }) => {
               <TableCell className="font-medium text-left">
                 Precio sobre Valor Contable (Price to Book)
               </TableCell>
-              <TableCell className="text-right">
-                {ratios.priceToBook}
-              </TableCell>
-            </TableRow>
-            <TableRow>
-              <TableCell className="font-medium text-left">
-                EV/EBITDA
-              </TableCell>
-              <TableCell className="text-right">{ratios.evEbitda}</TableCell>
-            </TableRow>
-            <TableRow>
-              <TableCell className="font-medium text-left">PEG</TableCell>
-              <TableCell className="text-right">{ratios.peg}</TableCell>
-            </TableRow>
-            <TableRow>
-              <TableCell className="font-medium text-left">ROA</TableCell>
-              <TableCell className="text-right">{ratios.roa}</TableCell>
+              <TableCell className="text-right">{ratios.pb}</TableCell>
             </TableRow>
             <TableRow>
               <TableCell className="font-medium text-left">ROE</TableCell>
               <TableCell className="text-right">{ratios.roe}</TableCell>
-            </TableRow>
-            <TableRow>
-              <TableCell className="font-medium text-left">
-                BPA (Beneficio por Acción)
-              </TableCell>
-              <TableCell className="text-right">{ratios.bpa}</TableCell>
-            </TableRow>
-            <TableRow>
-              <TableCell className="font-medium text-left">
-                DPA (Dividendo por Acción)
-              </TableCell>
-              <TableCell className="text-right">{ratios.dpa}</TableCell>
-            </TableRow>
-            <TableRow>
-              <TableCell className="font-medium text-left">Yield</TableCell>
-              <TableCell className="text-right">{ratios.yield}</TableCell>
-            </TableRow>
-            <TableRow>
-              <TableCell className="font-medium text-left">Payout</TableCell>
-              <TableCell className="text-right">{ratios.payout}</TableCell>
             </TableRow>
           </TableBody>
         </Table>
--- src/components/News.jsx
+++ src/components/News.jsx
@@ -1,24 +1,93 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
+import { Card, CardHeader, CardContent, CardFooter, CardTitle } from './ui/card';
+import { Badge } from './ui/badge';
+import { Skeleton } from './ui/skeleton';
+import { Button } from './ui/button';
+import { Dialog, DialogTrigger, DialogContent, DialogHeader, DialogTitle, DialogFooter } from './ui/dialog';
+import { Pagination } from '@/components/ui/pagination';
 
 function News({ news }) {
-  console.log(news);
-  return (
-    <div>
-      <div className="news-grid">
-        {!!news && news.slice(5).map((article, index) => (
-          <div> 
-            <div className="news-card">
-            <a href={article.url} target="_blank" rel="noopener noreferrer">
-              <img src={article.urlToImage} alt={article.title} className="news-image" />
-              <h3 className="news-title">{article.title}</h3>
-              <p className="news-description">{article.description}</p>
-            </a>
-          </div>
-          </div>
-        ))}
-      </div>
+  const [currentPage, setCurrentPage] = useState(1);
+  const itemsPerPage = 6;
 
+  console.log(news);
+
+  const indexOfLastItem = currentPage * itemsPerPage;
+  const indexOfFirstItem = indexOfLastItem - itemsPerPage;
+  const currentItems = news ? news.slice(indexOfFirstItem, indexOfLastItem) : [];
+
+  const totalPages = news ? Math.ceil(news.length / itemsPerPage) : 0;
+
+  return (
+    <div className="p-6">
+      <h1 className="text-3xl font-bold mb-4">Últimas Noticias</h1>
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+          {!!news && currentItems.map((article, index) => (
+            <Card key={index} className="hover:shadow-lg transition-shadow duration-300">
+              <CardHeader>
+                <img
+                  src={article.urlToImage}
+                  alt={article.title}
+                  className="w-full h-40 object-cover rounded-md"
+                />
+                <CardTitle className="mt-10">{article.title}</CardTitle>
+                <Badge variant="outline" className="mt-1">{article.source.name}</Badge>
+              </CardHeader>
+              <CardFooter className="flex justify-between">
+                <Dialog>
+                  <DialogTrigger asChild>
+                    <Button variant="outline">Ver más</Button>
+                  </DialogTrigger>
+                  <DialogContent>
+                    <DialogHeader>
+                      <DialogTitle>{article.title}</DialogTitle>
+                    </DialogHeader>
+                    <img
+                      src={article.urlToImage}
+                      alt={article.title}
+                      className="w-full h-64 object-cover rounded-md mb-4"
+                    />
+                    <p>{article.content || 'Contenido no disponible.'}</p>
+                    <DialogFooter>
+                      <Button asChild>
+                        <a
+                          href={article.url}
+                          target="_blank"
+                          rel="noopener noreferrer"
+                          className="text-blue-500 hover:underline"
+                        >
+                          Leer más en la fuente
+                        </a>
+                      </Button>
+                    </DialogFooter>
+                  </DialogContent>
+                </Dialog>
+              </CardFooter>
+            </Card>
+          ))}
+      </div>
+      {totalPages > 1 && (
+        <Pagination className="mt-8">
+          <Button
+            variant="outline"
+            onClick={() => setCurrentPage(prev => Math.max(prev - 1, 1))}
+            disabled={currentPage === 1}
+          >
+            Anterior
+          </Button>
+          <span className="mx-6 my-2">
+            Página {currentPage} de {totalPages}
+          </span>
+          <Button
+            variant="outline"
+            onClick={() => setCurrentPage(prev => Math.min(prev + 1, totalPages))}
+            disabled={currentPage === totalPages}
+          >
+            Siguiente
+          </Button>
+        </Pagination>
+      )}
     </div>
   );
 }
