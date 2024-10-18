bc00f4be950745adc9a72e5396457803996ed6ee
Jacito12 <jgarciaa@unitru.edu.pe>
2024-10-17 20:42:14 -0500

    Agregando mas datos para el Balance General

--- src/components/Balance_ratios.jsx
+++ src/components/Balance_ratios.jsx
@@ -7,107 +7,282 @@ import {
   TableHead,
   TableHeader,
   TableRow,
-} from "@/components/ui/table"
-import { Separator } from "@/components/ui/separator"
-import React, { useState } from 'react';
+} from "@/components/ui/table";
+import { Separator } from "@/components/ui/separator";
+import React from 'react';
 
-const Codigo = ({balance, ratios}) => {
+const BalanceFinanciero = ({ balance, ratios }) => {
   return (
     <div>
-      {
-        balance && (
-          <>
-            <Table>
-              <TableCaption>Balance Sheet Overview</TableCaption>
-              <TableHeader>
-                <TableRow>
-                  <TableHead className="w-[200px]">Item</TableHead>
-                  <TableHead className="text-right">Value (USD)</TableHead>
-                </TableRow>
-              </TableHeader>
-              <TableBody>
-                <TableRow>
-                  <TableCell className="font-medium text-left">Activos Corrientes</TableCell>
-                  <TableCell className="text-right">${balance.activosCorrientes.toLocaleString('en-US')}</TableCell>
-                </TableRow>
-                <TableRow>
-                  <TableCell className="font-medium text-left">Pasivos Corrientes</TableCell>
-                  <TableCell className="text-right">${balance.pasivosCorrientes.toLocaleString('en-US')}</TableCell>
-                </TableRow>
-                <TableRow>
-                  <TableCell className="font-medium text-left">Patrimonio</TableCell>
-                  <TableCell className="text-right">${balance.patrimonio.toLocaleString('en-US')}</TableCell>
-                </TableRow>
-              </TableBody>
-            </Table>
-            <Separator />
-          </>
-        )
-      }
-      
-      {ratios && (
-        <Table>
-        <TableCaption>Financial Ratios Overview</TableCaption>
-        <TableHeader>
-          <TableRow>
-            <TableHead className="w-[200px]">Ratio</TableHead>
-            <TableHead className="text-right">Value</TableHead>
-          </TableRow>
-        </TableHeader>
-        <TableBody>
-          <TableRow>
-            <TableCell className="font-medium text-left">PER</TableCell>
-            <TableCell className="text-right">{ratios.per}</TableCell>
-          </TableRow>
-          <TableRow>
-            <TableCell className="font-medium text-left">Precio sobre Ventas (PV)</TableCell>
-            <TableCell className="text-right">{ratios.pv}</TableCell>
-          </TableRow>
-          <TableRow>
-            <TableCell className="font-medium text-left">Precio sobre Valor Contable (Price to Book)</TableCell>
-            <TableCell className="text-right">{ratios.priceToBook}</TableCell>
-          </TableRow>
-          <TableRow>
-            <TableCell className="font-medium text-left">EV/EBITDA</TableCell>
-            <TableCell className="text-right">{ratios.evEbitda}</TableCell>
-          </TableRow>
-          <TableRow>
-            <TableCell className="font-medium text-left">PEG</TableCell>
-            <TableCell className="text-right">{ratios.peg}</TableCell>
-          </TableRow>
-          <TableRow>
-            <TableCell className="font-medium text-left">ROA</TableCell>
-            <TableCell className="text-right">{ratios.roa}</TableCell>
-          </TableRow>
-          <TableRow>
-            <TableCell className="font-medium text-left">ROE</TableCell>
-            <TableCell className="text-right">{ratios.roe}</TableCell>
-          </TableRow>
-          <TableRow>
-            <TableCell className="font-medium text-left">BPA (Beneficio por Acción)</TableCell>
-            <TableCell className="text-right">{ratios.bpa}</TableCell>
-          </TableRow>
-          <TableRow>
-            <TableCell className="font-medium text-left">DPA (Dividendo por Acción)</TableCell>
-            <TableCell className="text-right">{ratios.dpa}</TableCell>
-          </TableRow>
-          <TableRow>
-            <TableCell className="font-medium text-left">Yield</TableCell>
-            <TableCell className="text-right">{ratios.yield}</TableCell>
-          </TableRow>
-          <TableRow>
-            <TableCell className="font-medium text-left">Payout</TableCell>
-            <TableCell className="text-right">{ratios.payout}</TableCell>
-          </TableRow>
-        </TableBody>
-      </Table>
-      
+      {balance && (
+        <>
+          <Table>
+            <TableCaption>Balance Financiero</TableCaption>
+            <TableHeader>
+              <TableRow>
+                <TableHead className="w-[300px]">Item</TableHead>
+                <TableHead className="text-right">Valor (USD)</TableHead>
+              </TableRow>
+            </TableHeader>
+            <TableBody>
+              <TableRow>
+                <TableCell className="font-medium text-left" colSpan={2}>
+                  ACTIVO
+                </TableCell>
+              </TableRow>
+              <TableRow>
+                <TableCell className="text-left">Disponible</TableCell>
+                <TableCell className="text-right">
+                  ${balance.disponible.toLocaleString("en-US")}
+                </TableCell>
+              </TableRow>
+              <TableRow>
+                <TableCell className="text-left">
+                  Bancos y Otras Empresas del Sistema FII
+                </TableCell>
+                <TableCell className="text-right">
+                  ${balance.bancosFII.toLocaleString("en-US")}
+                </TableCell>
+              </TableRow>
+              <TableRow>
+                <TableCell className="text-left">Otras Disponibilidades</TableCell>
+                <TableCell className="text-right">
+                  ${balance.otrasDisponibilidades.toLocaleString("en-US")}
+                </TableCell>
+              </TableRow>
+              <TableRow>
+                <TableCell className="font-medium text-left">
+                  Cartera de Créditos
+                </TableCell>
+                <TableCell className="text-right">
+                  ${balance.carteraCreditos.toLocaleString("en-US")}
+                </TableCell>
+              </TableRow>
+              <TableRow>
+                <TableCell className="text-left">Créditos Vigentes</TableCell>
+                <TableCell className="text-right">
+                  ${balance.creditosVigentes.toLocaleString("en-US")}
+                </TableCell>
+              </TableRow>
+              <TableRow>
+                <TableCell className="text-left">
+                  Créditos en Cobranza Judicial
+                </TableCell>
+                <TableCell className="text-right">
+                  ${balance.creditosCobranza.toLocaleString("en-US")}
+                </TableCell>
+              </TableRow>
+              <TableRow>
+                <TableCell className="text-left">
+                  Provisiones para Créditos
+                </TableCell>
+                <TableCell className="text-right">
+                  (${balance.provisionesCreditos.toLocaleString("en-US")})
+                </TableCell>
+              </TableRow>
+              <TableRow>
+                <TableCell className="text-left">
+                  Inmuebles, Mobiliario y Equipo
+                </TableCell>
+                <TableCell className="text-right">
+                  ${balance.inmueblesEquipos.toLocaleString("en-US")}
+                </TableCell>
+              </TableRow>
+              <TableRow>
+                <TableCell className="font-medium text-left">
+                  Total Activo
+                </TableCell>
+                <TableCell className="text-right">
+                  ${balance.totalActivo.toLocaleString("en-US")}
+                </TableCell>
+              </TableRow>
 
+              <TableRow>
+                <TableCell className="font-medium text-left" colSpan={2}>
+                  PASIVO
+                </TableCell>
+              </TableRow>
+              <TableRow>
+                <TableCell className="text-left">
+                  Obligaciones con los Asociados
+                </TableCell>
+                <TableCell className="text-right">
+                  ${balance.obligacionesAsociados.toLocaleString("en-US")}
+                </TableCell>
+              </TableRow>
+              <TableRow>
+                <TableCell className="text-left">
+                  Obligaciones por Cuentas de Ahorro
+                </TableCell>
+                <TableCell className="text-right">
+                  ${balance.obligacionesAhorro.toLocaleString("en-US")}
+                </TableCell>
+              </TableRow>
+              <TableRow>
+                <TableCell className="text-left">
+                  Obligaciones por Cuenta a Plazo
+                </TableCell>
+                <TableCell className="text-right">
+                  ${balance.obligacionesPlazo.toLocaleString("en-US")}
+                </TableCell>
+              </TableRow>
+              <TableRow>
+                <TableCell className="text-left">Gastos por Pagar</TableCell>
+                <TableCell className="text-right">
+                  ${balance.gastosPorPagar.toLocaleString("en-US")}
+                </TableCell>
+              </TableRow>
+              <TableRow>
+                <TableCell className="text-left">Cuentas por Pagar</TableCell>
+                <TableCell className="text-right">
+                  ${balance.cuentasPorPagar.toLocaleString("en-US")}
+                </TableCell>
+              </TableRow>
+              <TableRow>
+                <TableCell className="text-left">
+                  Provisiones Asociativas
+                </TableCell>
+                <TableCell className="text-right">
+                  ${balance.provisionesAsociativas.toLocaleString("en-US")}
+                </TableCell>
+              </TableRow>
+              <TableRow>
+                <TableCell className="font-medium text-left">
+                  Total del Pasivo
+                </TableCell>
+                <TableCell className="text-right">
+                  ${balance.totalPasivo.toLocaleString("en-US")}
+                </TableCell>
+              </TableRow>
+
+              <TableRow>
+                <TableCell className="font-medium text-left" colSpan={2}>
+                  PATRIMONIO
+                </TableCell>
+              </TableRow>
+              <TableRow>
+                <TableCell className="text-left">Capital Social</TableCell>
+                <TableCell className="text-right">
+                  ${balance.capitalSocial.toLocaleString("en-US")}
+                </TableCell>
+              </TableRow>
+              <TableRow>
+                <TableCell className="text-left">Reservas</TableCell>
+                <TableCell className="text-right">
+                  ${balance.reservas.toLocaleString("en-US")}
+                </TableCell>
+              </TableRow>
+              <TableRow>
+                <TableCell className="text-left">
+                  Resultados Acumulados
+                </TableCell>
+                <TableCell className="text-right">
+                  ${balance.resultadosAcumulados.toLocaleString("en-US")}
+                </TableCell>
+              </TableRow>
+              <TableRow>
+                <TableCell className="text-left">
+                  Resultado Neto del Ejercicio
+                </TableCell>
+                <TableCell className="text-right">
+                  ${balance.resultadoEjercicio.toLocaleString("en-US")}
+                </TableCell>
+              </TableRow>
+              <TableRow>
+                <TableCell className="font-medium text-left">
+                  Total del Patrimonio
+                </TableCell>
+                <TableCell className="text-right">
+                  ${balance.totalPatrimonio.toLocaleString("en-US")}
+                </TableCell>
+              </TableRow>
+
+              <TableRow>
+                <TableCell className="font-medium text-left">
+                  Total del Pasivo y Patrimonio
+                </TableCell>
+                <TableCell className="text-right">
+                  ${balance.totalPasivoPatrimonio.toLocaleString("en-US")}
+                </TableCell>
+              </TableRow>
+            </TableBody>
+          </Table>
+
+          <Separator />
+        </>
       )}
 
-      
+      {ratios && (
+        <Table>
+          <TableCaption>Ratios Financieros</TableCaption>
+          <TableHeader>
+            <TableRow>
+              <TableHead className="w-[200px]">Ratio</TableHead>
+              <TableHead className="text-right">Valor</TableHead>
+            </TableRow>
+          </TableHeader>
+          <TableBody>
+            <TableRow>
+              <TableCell className="font-medium text-left">PER</TableCell>
+              <TableCell className="text-right">{ratios.per}</TableCell>
+            </TableRow>
+            <TableRow>
+              <TableCell className="font-medium text-left">
+                Precio sobre Ventas (PV)
+              </TableCell>
+              <TableCell className="text-right">{ratios.pv}</TableCell>
+            </TableRow>
+            <TableRow>
+              <TableCell className="font-medium text-left">
+                Precio sobre Valor Contable (Price to Book)
+              </TableCell>
+              <TableCell className="text-right">
+                {ratios.priceToBook}
+              </TableCell>
+            </TableRow>
+            <TableRow>
+              <TableCell className="font-medium text-left">
+                EV/EBITDA
+              </TableCell>
+              <TableCell className="text-right">{ratios.evEbitda}</TableCell>
+            </TableRow>
+            <TableRow>
+              <TableCell className="font-medium text-left">PEG</TableCell>
+              <TableCell className="text-right">{ratios.peg}</TableCell>
+            </TableRow>
+            <TableRow>
+              <TableCell className="font-medium text-left">ROA</TableCell>
+              <TableCell className="text-right">{ratios.roa}</TableCell>
+            </TableRow>
+            <TableRow>
+              <TableCell className="font-medium text-left">ROE</TableCell>
+              <TableCell className="text-right">{ratios.roe}</TableCell>
+            </TableRow>
+            <TableRow>
+              <TableCell className="font-medium text-left">
+                BPA (Beneficio por Acción)
+              </TableCell>
+              <TableCell className="text-right">{ratios.bpa}</TableCell>
+            </TableRow>
+            <TableRow>
+              <TableCell className="font-medium text-left">
+                DPA (Dividendo por Acción)
+              </TableCell>
+              <TableCell className="text-right">{ratios.dpa}</TableCell>
+            </TableRow>
+            <TableRow>
+              <TableCell className="font-medium text-left">Yield</TableCell>
+              <TableCell className="text-right">{ratios.yield}</TableCell>
+            </TableRow>
+            <TableRow>
+              <TableCell className="font-medium text-left">Payout</TableCell>
+              <TableCell className="text-right">{ratios.payout}</TableCell>
+            </TableRow>
+          </TableBody>
+        </Table>
+      )}
     </div>
   );
 };
 
-export default Codigo;
+export default BalanceFinanciero;
--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -2,15 +2,15 @@ import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import axios from 'axios';
 
-
-export const Search = ({symbol, setSymbol, setBalance, setRatios, setNews}) => {
-    const apiKey = 'I868WEQE7KDWQE0Y';
+export const Search = ({ symbol, setSymbol, setBalance, setRatios, setNews }) => {
+    const apiKey = 'S43WKTZQ4NBOY2OK';
     const apikey_news = '75a1603eecb042598d28343256620698';
 
     const handleSubmit = async (event) => {
         event.preventDefault();
-        setSymbol(event.target[0].value);
-        const financialData = await getFinancialData(symbol);
+        const inputSymbol = event.target[0].value;
+        setSymbol(inputSymbol);
+        const financialData = await getFinancialData(inputSymbol);
         if (financialData) {
           setBalance(financialData);
           const calculatedRatios = calculateRatios(financialData);
@@ -19,62 +19,78 @@ export const Search = ({symbol, setSymbol, setBalance, setRatios, setNews}) => {
           setBalance(null);
           setRatios(null);
         }
-
-        await getArticles()
+        await getArticles(inputSymbol);
     }
-    
+
     const getFinancialData = async (symbol) => {
       const balanceEndpoint = `https://www.alphavantage.co/query?function=BALANCE_SHEET&symbol=${symbol}&apikey=${apiKey}`;
       const earningsEndpoint = `https://www.alphavantage.co/query?function=INCOME_STATEMENT&symbol=${symbol}&apikey=${apiKey}`;
       const overviewEndpoint = `https://www.alphavantage.co/query?function=OVERVIEW&symbol=${symbol}&apikey=${apiKey}`;
-      
+
       try {
         const balanceResponse = await axios.get(balanceEndpoint);
         const earningsResponse = await axios.get(earningsEndpoint);
         const overviewResponse = await axios.get(overviewEndpoint);
-    
+
         console.log('Respuesta del balance:', balanceResponse.data);
         console.log('Respuesta de ingresos:', earningsResponse.data);
         console.log('Respuesta de overview:', overviewResponse.data);
-    
+
         if (!balanceResponse.data.annualReports || balanceResponse.data.annualReports.length === 0) {
           console.error('No se encontraron datos de balance para esta empresa');
           return null;
         }
+
         if (!earningsResponse.data.annualReports || earningsResponse.data.annualReports.length === 0) {
           console.error('No se encontraron datos de ingresos para esta empresa');
           return null;
         }
-    
+
         const balance = balanceResponse.data.annualReports[0];
         const earnings = earningsResponse.data.annualReports[0];
         const overview = overviewResponse.data;
-    
+
+        if (!balance || !earnings) {
+          console.error('No se encontraron datos para esta empresa');
+          return null;
+        }
+
         return {
-          activosCorrientes: balance.totalCurrentAssets || 0,
-          pasivosCorrientes: balance.totalCurrentLiabilities || 0,
-          activosNoCorrientes: balance.totalNonCurrentAssets || 0,
-          pasivosNoCorrientes: balance.totalNonCurrentLiabilities || 0,
-          pasivosTotales: balance.totalLiabilities || 0,
-          patrimonio: balance.totalShareholderEquity || 0,
-          ebitda: earnings.ebitda || 0,
-          netIncome: earnings.netIncome || 0,
-          sharesOutstanding: overview.SharesOutstanding || 0,
-          dividendPerShare: overview.DividendPerShare || 0,
-          price: overview.MarketCapitalization / (overview.SharesOutstanding || 1),
+          disponible: balance.cashAndCashEquivalentsAtCarryingValue || 0,
+          bancosFII: balance.shortTermInvestments || 0,
+          otrasDisponibilidades: balance.cash || 0,
+          carteraCreditos: balance.netReceivables || 0,
+          creditosVigentes: balance.totalCurrentAssets || 0,
+          creditosCobranza: balance.currentNetReceivables || 0,
+          provisionesCreditos: balance.totalCurrentLiabilities || 0,
+          inmueblesEquipos: balance.propertyPlantEquipment || 0,
+          totalActivo: balance.totalAssets || 0,
+          obligacionesAsociados: balance.totalCurrentLiabilities || 0,
+          obligacionesAhorro: balance.shortTermDebt || 0,
+          obligacionesPlazo: balance.longTermDebt || 0,
+          gastosPorPagar: earnings.operatingExpenses || 0,
+          cuentasPorPagar: balance.accountsPayable || 0,
+          provisionesAsociativas: balance.deferredRevenue || 0,
+          totalPasivo: balance.totalLiabilities || 0,
+          capitalSocial: balance.commonStock || 0,
+          reservas: balance.retainedEarnings || 0,
+          resultadosAcumulados: balance.accumulatedOtherComprehensiveIncomeLoss || 0,
+          resultadoEjercicio: earnings.netIncome || 0,
+          totalPatrimonio: balance.totalShareholderEquity || 0,
+          totalPasivoPatrimonio: balance.totalLiabilitiesAndShareholdersEquity || 0,
           peRatio: overview.PERatio || 0,
           priceToBook: overview.PriceToBookRatio || 0,
-          revenue: earnings.totalRevenue || 0,
-          growthRate: overview.EPSGrowth5Y || 1,
+          dividendPerShare: overview.DividendPerShare || 0,
           dividendYield: overview.DividendYield || 0,
-          payoutRatio: overview.PayoutRatio || 0
+          payoutRatio: overview.PayoutRatio || 0,
+          growthRate: overview.RevenueGrowth || 0, 
         };
       } catch (error) {
         console.error('Error en la API:', error);
         return null;
       }
     };
-    
+
     const calculateRatios = (data) => {
       const ratios = {};
       ratios.per = data.peRatio;
@@ -92,21 +108,17 @@ export const Search = ({symbol, setSymbol, setBalance, setRatios, setNews}) => {
       return ratios;
     }
 
-    
-
-    const getArticles = async () => {
+    const getArticles = async (symbol) => {
         axios.get(`https://newsapi.org/v2/everything?q=${symbol}&language=es&sortBy=publishedAt&apiKey=${apikey_news}`)
         .then(response => {
             setNews(response.data.articles);
             console.log('Noticias:', response.data.articles);
         })
         .catch(error => {
-            setError('Error al obtener las noticias.');
-            console.error('Error al hacer la solicitud:', error);
+            console.error('Error al hacer la solicitud de noticias:', error);
         });
     }
 
-        
     return (
         <form className="flex w-full max-w-sm items-center space-x-2" onSubmit={handleSubmit}>
             <Input type="text" placeholder="Busqueda"/>
@@ -114,7 +126,3 @@ export const Search = ({symbol, setSymbol, setBalance, setRatios, setNews}) => {
         </form>
     )
 }
-
-
-
-
