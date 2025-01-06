ac06376f834576b7451568b3d52171155c6e2dce
georgeacp <133732710+georgeacp@users.noreply.github.com>
2025-01-05 21:16:48 -0500

    feat: update header to make the title clickable

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,28 @@
-import './App.css';
-import { CalculatorGrid } from './components/CalculatorGrid';
-import { Search } from './components/Search';
-import { SearchProvider } from './context/search.context';
-import { EnterprisePage } from './pages/Enterprise';
+import "./App.css";
+import { CalculatorGrid } from "./components/CalculatorGrid";
+import { Search } from "./components/Search";
+import { SearchProvider } from "./context/search.context";
+import { EnterprisePage } from "./pages/Enterprise";
 
 function App() {
   return (
     <SearchProvider>
       <div className="space-y-10">
         <header className="text-center space-y-4 w-full">
-          <h1 className="text-4xl font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-blue-500 to-green-500 drop-shadow-lg">
+          <a
+            href="/"
+            className="text-4xl font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-blue-500 to-green-500 drop-shadow-lg"
+          >
             Dashboard Financiero
-          </h1>
+          </a>
           <p className="text-lg text-gray-600">
-            Realiza cálculos financieros avanzados y consulta datos relevantes en tiempo real.
+            Realiza cálculos financieros avanzados y consulta datos relevantes
+            en tiempo real.
           </p>
           <Search />
         </header>
         <main>
-          <CalculatorGrid /> 
+          <CalculatorGrid />
           <EnterprisePage />
         </main>
       </div>
